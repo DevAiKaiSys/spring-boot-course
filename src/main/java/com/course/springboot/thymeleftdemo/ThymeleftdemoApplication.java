328e0b2ae89d0ce05980bebd4651caf7606ac920
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-07-26 19:45:05 +0700

    174. Spring Boot - Spring MVC with Thymeleaf - Coding - Part 1

---
+++ src/main/java/com/course/springboot/thymeleftdemo/ThymeleftdemoApplication.java
@@ -0,0 +1,13 @@
+package com.course.springboot.thymeleftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleftdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleftdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/springboot/thymeleftdemo/ThymeleftdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.springboot.thymeleftdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleftdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
