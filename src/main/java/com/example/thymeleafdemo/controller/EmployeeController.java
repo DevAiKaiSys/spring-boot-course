a9b5b0e462627f8dbd99ae84a5796c825f4089b8
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-07-27 18:08:00 +0700

    238. CRUD Database Project - Update Employee - Coding

--- src/main/java/com/example/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/example/thymeleafdemo/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.example.thymeleafdemo.entity.Employee;
 import com.example.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,6 +39,19 @@ public class EmployeeController {
 
         theModel.addAttribute("employee", theEmployee);
 
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+
+        // get the employee from the service
+        Employee theEmployee = employeeService.findById(theId);
+
+        // set employee in the model to prepopulate the form
+        theModel.addAttribute("employee", theEmployee);
+
+        // send over to out form
         return "employees/employee-form";
     }
 
